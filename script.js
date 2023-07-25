86f6ed7461d7a177dea42d6863e99074418024ec
Anira Uoren <airin6666@yandex.ru>
2023-07-25 14:33:27 +0300

    feat: Работа модальных окон - Поля формы
    bindProfileFields()
    
    Closes WP-113 Работа модальных окон - Поля формы

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
+/*Попапы*/
 const overlayElement = document.querySelector(".overlay");
 const editProfilePopupElement = document.querySelector(".popup__edit-profile");
 const addPlacePopupElement = document.querySelector(".popup__add-place");
 const viewImagePopupElement = document.querySelector(".popup__image-viewer");
 
+/*Кнопки для попапов*/
 const editProfileBtn = document.querySelector(".profile__edit-btn");
 const addPlaceBtn = document.querySelector(".profile__add-btn");
+
+/*Поля профиль пользователя*/
+const profileNameElement = document.querySelector(".profile__name");
+const profileAboutElement = document.querySelector(".profile__about");
+
+/**
+ * Функция для простого навешивания слушателей на попап для его открытия и закрытия.
+ * @param button {Element} - кнопка открытия попапа;
+ * @param popup {Element} - попап, который следует открыть при нажатии на button.
+ */
 function addListenerOnBtn(button, popup) {
   const closeBtn = popup.querySelector(".popup__close-btn");
 
@@ -19,5 +31,28 @@ function addListenerOnBtn(button, popup) {
   })
 }
 
+/**
+ * Функция связывает поля профиля с полями фармф редактирования профиля
+ */
+function bindProfileFields() {
+  const nameAboutValue = profileNameElement.textContent;
+  const aboutProfileValue = profileAboutElement.textContent;
+
+  console.log(profileNameElement)
+  console.log(profileAboutElement)
+  console.log(nameAboutValue)
+  console.log(aboutProfileValue)
+
+  const nameProfileInput= editProfilePopupElement.querySelector("#personName");
+  const aboutProfileInput= editProfilePopupElement.querySelector("#personAbout");
+
+  if (nameAboutValue && aboutProfileValue) {
+    nameProfileInput.value = nameAboutValue;
+    aboutProfileInput.value = aboutProfileValue;
+  }
+}
+
+/*TODO НАдо собрать по попапам в отдельную функцию навешивание логики, слушателей и т.п.*/
 addListenerOnBtn(editProfileBtn, editProfilePopupElement);
 addListenerOnBtn(addPlaceBtn, addPlacePopupElement);
+bindProfileFields();
