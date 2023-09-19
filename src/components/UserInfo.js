7b85e67d214729a78376b772fdd01d6a0855e313
Anira Uoren <airin6666@yandex.ru>
2023-09-19 19:27:34 +0300

    feat: Class UserInfo
    Добавлен рендер полученных данных о пользователе.
    
    Closes WP-209 UserInfo

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,26 +1,22 @@
-class UserInfo {
+export class UserInfo {
   constructor(nameSelector, aboutSelector, avatarSelector) {
-    this._nameSelector = nameSelector;
-    this._aboutSelector = aboutSelector;
-    this._avatarSelector = avatarSelector;
+    this._nameElement = document.querySelector(nameSelector);
+    this._aboutElement = document.querySelector(aboutSelector);
+    this._avatarAvatar = document.querySelector(avatarSelector);
+  }
+
+  render(data) {
+    this._nameElement.textContent = data.name;
+    this._aboutElement.textContent = data.about;
+    this._avatarAvatar.src = data.avatar;
+    this._avatarAvatar.alt = data.name;
   }
 
   getUserInfo(handler) {
-    handler()
-      .then(data => {
-        return data;
-        //TODO Кажется, что метод должен еще и подставлять на страницу
-      })
-      .catch(err => {
-        console.error(err);
-      })
+    handler();
   }
 
   setUserInfo({name, about}, handler) {
-    handler(name, about)
-      .then(data => {
-        return data;
-        //TODO Кажется, что метод должен еще и подставлять на страницу
-      })
+    handler(name, about);
   }
 }
